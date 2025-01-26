9cea0f4189381f5644ee8f4cae2fc51be35f6665
wagadao.y <wagadao.y@outlook.com>
2025-01-26 08:44:50 +0000

    a

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <h1>トップページ</h1>
 <a href="/page1">ページ1</a>
 <Button onclick={getHello} color="blue">Get HelloWorld</Button>
-<Button onclick={postHello} color="blue">Post HelloWorld</Button>
-<Button onclick={getValidateTest} color="blue">Get Validate Test</Button>
-<Button onclick={getValidateTest} color="blue">Get Validate Test</Button>
-<Button onclick={getValidateHello} color="blue">Get Validate Hello</Button>
+<Button onclick={postHello} color="green">Post HelloWorld</Button>
+<Button onclick={getValidateTest} color="yellow">Get Validate Test</Button>
+<Button onclick={getValidateTest} color="red">Get Validate Test</Button>
+<Button onclick={getValidateHello} color="violet">Get Validate Hello</Button>
